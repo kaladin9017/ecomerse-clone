0e87afed5c4f202dca57a97ef0255b61edd098cb
kaladin9017 <sanchochris@gmail.com>
2017-04-02 13:39:22 -0400

    added connection to mongodb and added boilerplate for user schema

---
+++ models/user.js
@@ -0,0 +1,50 @@
+// USER SCHEMA
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
+const Schema = mongoose.Schema;
+// First Create User Schema attributes
+
+const UserSchema = new Schema({
+  email: { type: String, unique: true, lowercase: true },
+  password: { type: String, },
+
+  profile: {
+    name: { type: String, default: ''},
+    picture: { type: String, default: '' },
+  },
+
+  address: { type: String },
+
+  history: [{
+    date: Date,
+    paid: { type: Number, default: 0 },
+    // items: { type: Schema.Types.ObjectId, ref: '' }
+  }]
+})
+
+
+
+// User methods
+// Hash passwords before saving to db
+UserSchema.pre('save', function(next) {
+  let user = this;
+  if (!user.isModified('password')) return next();
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) return next(err);
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+
+})
+
+
+// Compare password in db to the user input
+UserSchema.methods.comparePassoword = function(password) {
+  return bcrypt.compareSync(password, this.password);
+}
+
+
+module.exports = mongoose.model('User', UserSchema);
--- server.js
+++ server.js
@@ -1,16 +1,35 @@
 const express = require('express');
-const bodyparser = require('body-parser');
-const path = require('path');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 let app = express();
 
-app.use(bodyparser.urlencoded({ extended: false }));
-app.use(bodyparser.json());
-app.use(express.static('public'))
+const bodyparser = require('body-parser');
+const path = require('path');
 
+mongoose.connect('mongodb://root:Queenstech@ds147920.mlab.com:47920/ecommerse', function(err) {
+  if (err) {
+    console.log(err);
+  }
+  else {
+    console.log('connected to db');
+  }
+})
+
+
+// Middleare
+app.use(morgan('dev'));
+app.use(express.static('public'));
+// app.use(bodyparser.urlencoded({ extended: false }));
+// app.use(bodyparser.json());
+
+
+app.get('/catname', function(req,res) {
+  res.json('batman');
+});
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-})
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
 
 
 app.listen(3000);
